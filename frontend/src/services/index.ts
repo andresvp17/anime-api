222ec2e19de4b205c556d62954a97e3a5c8765eb
andresvp17 <andresvp257@gmail.com>
2024-05-22 23:24:19 -0400

    allowed netlify origin

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://anime-api-production-d0f2.up.railway.app/animes'
 
 export const getInput = async (input: string) => {
-    return fetch(`${BASE_URL}${input}`)
+    return fetch(`${BASE_URL  }${input}`)
       .then((res) => {
         if (!res.ok) {
             throw { error: true, message: 'Something went wrong!' }
