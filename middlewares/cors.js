4f440c02345158abcaf3c9a81cf7f0f65885a0fe
andresvp17 <andresvp257@gmail.com>
2024-05-22 23:25:01 -0400

    netlify allowed

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
   'http://localhost:1234',
   'http://localhost:8080',
-  'http://localhost:4321'
+  'http://localhost:4321',
+  'https://main--apinime.netlify.app/'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => {
